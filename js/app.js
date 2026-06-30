3055ee19f89e824f716a4a2de62cc362d2f21614
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-06-30 14:00:25 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzsBlbmfyzecmKurNXbyz4oFCEvV9y472P4xbiba-gvE9a3yOSmzNHvF_aSe0HEMrt0/exec";
+const API_URL =
+"https://script.google.com/macros/s/AKfycbzsBlbmfyzecmKurNXbyz4oFCEvV9y472P4xbiba-gvE9a3yOSmzNHvF_aSe0HEMrt0/exec";
+
+const API_TOKEN =
+"CONGLENH_TANHIEP_2026";
