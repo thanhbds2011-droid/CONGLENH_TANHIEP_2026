95971654086d3fdc6784d52ff4f297b56daf1183
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-03 17:16:13 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "cong-lenh-cache-v120";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request, { cache: "no-store" })
+      .catch(() => caches.match(event.request))
+  );
+});
