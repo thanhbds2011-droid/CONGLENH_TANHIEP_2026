82079582c786eb7396b8fe421cf9d327ee24aa1d
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-03 22:26:15 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbzsBlbmfyzecmKurNXbyz4oFCEvV9y472P4xbiba-gvE9a3yOSmzNHvF_aSe0HEMrt0/exec";
 const API_TOKEN = "CONGLENH_TANHIEP_2026";
-const CURRENT_VERSION = "119";
+const CURRENT_VERSION = "120";
 
 let DU_LIEU_NHAT_KY = [];
 
